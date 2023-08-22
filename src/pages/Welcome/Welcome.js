4aafdc98903069d6a2caa88c36e63046f01fad64
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2023-08-22 20:49:36 +0500

    admob [ADD]

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,5 +1,5 @@
 // @app
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   View,
   ImageBackground,
@@ -8,10 +8,47 @@ import {
 
 import { styles } from './styles';
 import Button from '../../components/Button';
-
+import { AppOpenAd, InterstitialAd, RewardedAd, BannerAd, TestIds, BannerAdSize, AdEventType, RewardedAdEventType } from 'react-native-google-mobile-ads';
+const adUnitId = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-3340464236132360/1338586768';
+console.log(adUnitId,'adUnitId')
+const interstitial = InterstitialAd.createForAdRequest(adUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fashion', 'clothing'],
+});
 const Welcome = ({ navigation }) => {
+
+  useEffect(async () => {
+    // await screenTrace()
+    // crashlytics().log('code mounted.');
+    // crashlytics().crash();
+    // const trace = await perf().startTrace('get_Code');
+
+    // setCodeForUI()
+
+    // await trace.stop();
+    const unsubscribe = interstitial.addAdEventListener(AdEventType.LOADED, () => {
+      interstitial.show();
+    });
+    // Start loading the interstitial straight away
+    interstitial.load();
+    // Unsubscribe from events on unmount
+    return unsubscribe;
+
+  }, [])
   return (
     <View style={styles.container}>
+      {/* <View style={{ width: '100%', alignItems: 'center' }}>
+
+        <BannerAd
+          size={BannerAdSize.BANNER}
+          // unitId={'ca-app-pub-3340464236132360/7129135347'}
+          unitId={adUnitId}
+          // unitId={"ca-app-pub-9152919921144751/4080981743"} 
+          requestOptions={{
+            requestNonPersonalizedAdsOnly: true,
+          }}
+        />
+      </View> */}
       <ImageBackground
         style={styles.welcomeBg}
         resizeMode='cover'
